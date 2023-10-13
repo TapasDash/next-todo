f3ab7df4b7bab229249103b210e6073382615445
TapasDash <tapasdash017@gmail.com>
2023-10-14 00:03:53 +0530

    fix

--- app/page.js
+++ app/page.js
@@ -3,9 +3,6 @@ import { headers } from "next/headers";
 import Image from "next/image";
 
 export default function Home() {
-  const headersList = headers();
-  // read the custom x-url header
-  console.log({ headersList });
   // console.log(header_url);
   return (
     <>
